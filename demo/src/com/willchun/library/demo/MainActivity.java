83ecca4a26e4f22e7b5d0addf21868234f0c2b0e
willchun <277143980@qq.com>
2013-12-27 11:30:30 +0800

    增加了 ToastView的测试

--- demo/src/com/willchun/library/demo/MainActivity.java
+++ demo/src/com/willchun/library/demo/MainActivity.java
@@ -7,15 +7,18 @@
 package com.willchun.library.demo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 import com.willchun.library.base.AndActivity;
 import com.willchun.library.service.NetworkStateService;
+import com.willchun.library.view.ToastView;
 
 /**
  *@author chunwang (wcly10@gmail.com)
  *@date 2013-12-26
  */
-public class MainActivity extends AndActivity {
+public class MainActivity extends AndActivity implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedState) {
@@ -24,6 +27,30 @@ public class MainActivity extends AndActivity {
         setContentView(R.layout.main);
         
         startService(intent(NetworkStateService.class));
+        
+        aq.id(R.id.showbtn).clicked(this);
     }
+
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        stopService(intent(NetworkStateService.class));
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        switch (v.getId()) {
+        case R.id.showbtn:
+            ToastView.show(this, R.string.app_name, R.drawable.toast_image);
+            break;
+
+        default:
+            break;
+        }
+    }
+    
+    
     
 }
