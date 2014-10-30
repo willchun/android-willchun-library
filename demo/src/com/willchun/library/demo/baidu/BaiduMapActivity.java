7ef96e1a70a4e82cda7fddea13fe56ee7d3414a5
willchun <277143980@qq.com>
2014-10-30 18:11:12 +0800

    百度地图 增加了 覆盖物

--- demo/src/com/willchun/library/demo/baidu/BaiduMapActivity.java
+++ demo/src/com/willchun/library/demo/baidu/BaiduMapActivity.java
@@ -15,6 +15,8 @@ import com.willchun.library.base.AndQuery;
 import com.willchun.library.demo.R;
 import com.willchun.library.platform.baidu.BaiduPlatformLocationAgent;
 
+import java.util.ArrayList;
+
 
 /**
  * Created by Administrator on 2014/9/1.
@@ -26,8 +28,14 @@ public class BaiduMapActivity extends AndActivity implements CompoundButton.OnCh
     private BaiduMap mBaiduMap = null;
     //定位模式
     private MyLocationConfiguration.LocationMode mCurrentLocationMode;
-    //当前地图的图片，自定义
+
+
+    //当前定位点的自定义图片
+    private BitmapDescriptor mCustomLocBitmap;
+    //当前地图的定位图片
     private BitmapDescriptor mCurrentMarker;
+    //覆盖物组
+    private ArrayList<Marker> mMarkers = new ArrayList<Marker>();
 
     private boolean isFirstLoc = true;
 
@@ -50,15 +58,24 @@ public class BaiduMapActivity extends AndActivity implements CompoundButton.OnCh
         // 开启定位图层
         mBaiduMap.setMyLocationEnabled(true);
 
+        //
+
 
         initLocation();
+        initMulch();
     }
 
     private void init(){
         ((RadioButton) findViewById(R.id.type_map_normal_rb)).setOnCheckedChangeListener(this);
         ((RadioButton)findViewById(R.id.type_map_satellite_rb)).setOnCheckedChangeListener(this);
         aq.id(R.id.traffic_cb).getCheckBox().setOnCheckedChangeListener(this);
+        aq.id(R.id.custom_loc_icon_cb).getCheckBox().setOnCheckedChangeListener(this);
+        aq.id(R.id.mulch_cb).getCheckBox().setOnCheckedChangeListener(this);
         aq.id(R.id.loc_mode_btn).clicked(this);
+
+        //自定义的定位点图片
+        mCustomLocBitmap = BitmapDescriptorFactory.fromResource(R.drawable.icon_geo);
+
     }
 
     private void initLocation(){
@@ -95,9 +112,15 @@ public class BaiduMapActivity extends AndActivity implements CompoundButton.OnCh
         });
 
         BaiduPlatformLocationAgent.getInstance(aq.getContext()).startLocation();
+    }
+
+
+    private void initMulch() {
 
 
     }
+
+
 
     @Override
     protected void onPause() {
@@ -116,6 +139,7 @@ public class BaiduMapActivity extends AndActivity implements CompoundButton.OnCh
         BaiduPlatformLocationAgent.getInstance(aq.getContext()).stopLocation();
         mBaiduMap.setMyLocationEnabled(false);
         mMapView.onDestroy();
+        mMapView = null;
         super.onDestroy();
 
     }
@@ -138,10 +162,89 @@ public class BaiduMapActivity extends AndActivity implements CompoundButton.OnCh
                 mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
                 break;
             case R.id.type_map_satellite_rb:
-                //卫星地图
                 if(isChecked)
                 mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
                 break;
+            //自定义定位图标
+            case R.id.custom_loc_icon_cb:
+                if(isChecked){
+                    mCurrentMarker = mCustomLocBitmap;
+                }else{
+                    mCurrentMarker = null;
+                }
+                mBaiduMap
+                        .setMyLocationConfigeration(new MyLocationConfiguration(
+                                mCurrentLocationMode, true, mCurrentMarker));
+                break;
+            //覆盖物
+            case R.id.mulch_cb:
+                if(isChecked){
+                    double offer = 0.05;
+                    MyLocationData myData = mBaiduMap.getLocationData();
+                    BitmapDescriptor bd = BitmapDescriptorFactory.fromResource(R.drawable.map_mulch);
+                    OverlayOptions oo1 = new MarkerOptions()
+                            .position(new LatLng(myData.latitude + offer, myData.longitude + offer))//经纬度信息
+                            .icon(bd)//覆盖物图片
+                            .title("覆盖物A")//覆盖物标题
+                            .zIndex(9)//覆盖物层级
+                            .draggable(true);//设置覆盖物是否能拖拽
+
+                    Marker mMulchMarker1 = (Marker)(mBaiduMap.addOverlay(oo1));
+                    mMarkers.add(mMulchMarker1);
+
+                    OverlayOptions oo2 = new MarkerOptions()
+                            .position(new LatLng(myData.latitude - offer, myData.longitude + offer))//经纬度信息
+                            .icon(bd)//覆盖物图片
+                            .title("覆盖物A")//覆盖物标题
+                            .zIndex(9)//覆盖物层级
+                            .draggable(true);//设置覆盖物是否能拖拽
+
+                    Marker mMulchMarker2 = (Marker)(mBaiduMap.addOverlay(oo2));
+                    mMarkers.add(mMulchMarker2);
+
+                    OverlayOptions oo3 = new MarkerOptions()
+                            .position(new LatLng(myData.latitude - offer, myData.longitude - offer))//经纬度信息
+                            .icon(bd)//覆盖物图片
+                            .title("覆盖物A")//覆盖物标题
+                            .zIndex(9)//覆盖物层级
+                            .draggable(true);//设置覆盖物是否能拖拽
+
+                    Marker mMulchMarker3 = (Marker)(mBaiduMap.addOverlay(oo3));
+                    mMarkers.add(mMulchMarker3);
+
+                    mBaiduMap.setOnMarkerDragListener(new BaiduMap.OnMarkerDragListener() {
+                        public void onMarkerDrag(Marker marker) {
+                            //拖拽中
+                            Log.i("mulch", "onMarkerDrag");
+                        }
+                        public void onMarkerDragEnd(Marker marker) {
+                            //拖拽结束
+                            Log.i("mulch", "onMarkerDragEnd");
+                        }
+                        public void onMarkerDragStart(Marker marker) {
+                            //开始拖拽
+                            Log.i("mulch", "onMarkerDragStart");
+                        }
+                    });
+
+
+                }else{
+                    /*if(mMulchMarker != null){
+                        //调用Marker对象的remove方法实现指定marker的删除
+                        mMulchMarker.remove();
+                    }
+
+                    mMulchMarker = null;*/
+
+                    for(Marker marker : mMarkers){
+                        marker.remove();
+                    }
+
+                    mMarkers.clear();
+                }
+                break;
+
+
         }
     }
 
